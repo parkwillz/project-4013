d5db16a4fae0f412a0dfc0383f86c90be66d9ae1
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-19 15:35:54 -0600

    Update widereceivers.php

--- widereceivers.php
+++ widereceivers.php
@@ -1,1 +1,38 @@
+<?php
+require_once("util-db.php");
+require_once("model-widereceivers.php");
 
+$pageTitle = "Wide Receivers";
+include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertWidereceivers($_POST['wName'], $_POST['wRec'], $_POST['wYards'], $_POST['wTDs'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Added!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateWidereceivers($_POST['wName'], $_POST['wRec'], $_POST['wYards'], $_POST['wTDs'], $_POST['wrid'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Edited!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteWidereceivers($_POST['wrid'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Deleted!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+$widereceivers = selectWidereceivers();
+include "view-widereceivers.php";
+include "view-footer.php";
+?>
