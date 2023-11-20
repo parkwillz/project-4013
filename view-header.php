5efc83c1ef5bfa07d483dfa0e61fc3327439dbe0
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-19 18:55:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
+    <link rel = "stylesheet" href = "bootstrap.css">
+    <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
       body{
@@ -17,6 +19,7 @@
 
       }
     </style>
+    -->
   </head>
   <body>
     <div class ="container">
