dca1571346c63524200fd7a3a623c960aa993485
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-19 19:13:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>*Placeholder*</h1>
+<div class="text-center">
+    <h1 class="display-4">Welcome to our NFL Database</h1>
+    <p>On this site you can view offensive skill position statistics for QBs, WRs and RBs</p>
+</div>
+
 <?php
 include "view-footer.php";
 ?>
