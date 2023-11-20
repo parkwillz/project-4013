04bfc7168377cb01ae5252e748470b916523156a
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-19 19:24:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,18 @@ include "view-header.php";
     <p>On this site you can view offensive skill position statistics for QBs, WRs and RBs</p>
 </div>
 
+<div class="row mt-5 d-flex justify-content-center">
+    <div class="col-md-4 d-flex align-items-center">
+        <img src="/Content/hurts.jpeg" class="img-fluid" alt="Jalen Hurts">
+    </div>
+    <div class="col-md-2 d-flex flex-column justify-content-center">
+        <img src="~/Content/NFL-logo.png" class="img-fluid" />
+    </div>
+    <div class="col-md-4 d-flex flex-column justify-content-center">
+        <img src="/Content/waddle.jpeg" class="img-fluid" alt="Jaylen Waddle">
+    </div>
+</div>
+
 <?php
 include "view-footer.php";
 ?>
