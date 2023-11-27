d570cd72f68cb4afe2eb531bb7cd042a42006bd1
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-27 13:59:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>-->
 <!--<script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js"></script>-->
 <div class="text-center">
     <h1 class="display-4">NFL Database</h1>
@@ -20,11 +20,34 @@ include "view-header.php";
         <img src="/Contents/waddle.jpeg" class="img-fluid" alt="Jaylen Waddle">
     </div>
 </div>
-<h1 class="text-center" style="margin-top:70px">
+<h1 class="ml6">
   <span class="text-wrapper">
-    <span class="letters">Click on a skill position to view their stats</span>
+    <span class="letters">Beautiful Questions</span>
   </span>
 </h1>
+<style>
+    .ml6 {
+  position: relative;
+  font-weight: 900;
+  font-size: 3.3em;
+}
+
+.ml6 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.2em;
+  padding-right: 0.05em;
+  padding-bottom: 0.1em;
+  overflow: hidden;
+}
+
+.ml6 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
+</style>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
 <!--
 <div class="text-center" style="margin-top:70px">
     <p>Click on a skill position to view their stats</p>
@@ -42,18 +65,19 @@ include "view-header.php";
 </script>
 
 <script>
-var textWrapper = document.querySelector('.text-center .letters');
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.text-center .letter',
+    targets: '.ml6 .letter',
     translateY: ["1.1em", 0],
     translateZ: 0,
     duration: 750,
     delay: (el, i) => 50 * i
   }).add({
-    targets: '.text-center',
+    targets: '.ml6',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
