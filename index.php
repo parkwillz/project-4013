b90712c48c53f8369c4da82afcd980497dd754ab
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-27 14:07:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,20 @@ include "view-header.php";
 ?>
 <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>-->
 <!--<script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js"></script>-->
+<!--
 <div class="text-center">
     <h1 class="display-4">NFL Database</h1>
+    <p>Welcome! On this site you can view offensive skill position statistics for QBs, WRs and RBs</p>
+</div>
+-->
+<h1 class="ml1">
+  <span class="text-wrapper">
+    <span class="line line1"></span>
+    <span class="letters">NFL Database</span>
+    <span class="line line2"></span>
+  </span>
+</h1>
+<div class="text-center">
     <p>Welcome! On this site you can view offensive skill position statistics for QBs, WRs and RBs</p>
 </div>
 
@@ -46,6 +58,37 @@ include "view-header.php";
   display: inline-block;
   line-height: 1em;
 }
+
+.ml1 {
+  font-weight: 900;
+  font-size: 3.5em;
+}
+
+.ml1 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
+
+.ml1 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml1 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 3px;
+  width: 100%;
+  background-color: #fff;
+  transform-origin: 0 0;
+}
+
+.ml1 .line1 { top: 0; }
+.ml1 .line2 { bottom: 0; }
 </style>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
@@ -85,6 +128,36 @@ anime.timeline({loop: true})
     delay: 1000
   });
     </script>
+
+<script>
+    var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+</script>
 
 <?php
 include "view-footer.php";
