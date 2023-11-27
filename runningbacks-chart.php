773b2a9021a64b6f77759c1effa3617e44d37e77
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-27 16:24:55 -0600

    Update runningbacks-chart.php

--- runningbacks-chart.php
+++ runningbacks-chart.php
@@ -1,1 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model-runningbacks.php");
 
+$pageTitle = "Runningbacks Chart";
+include "view-header.php";
+
+$runningbacks = selectRunningbacks();
+include "view-runningbacks.php";
+include "view-footer.php";
+?>
