3384f568d7e28ad6265810dcb88d65fe983fe200
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-11-27 13:45:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
 <div class="text-center">
     <h1 class="display-4">NFL Database</h1>
     <p>Welcome! On this site you can view offensive skill position statistics for QBs, WRs and RBs</p>
@@ -19,9 +20,16 @@ include "view-header.php";
         <img src="/Contents/waddle.jpeg" class="img-fluid" alt="Jaylen Waddle">
     </div>
 </div>
+<h1 class="text-center" style="margin-top:70px">
+  <span class="text-wrapper">
+    <span class="letters">Click on a skill position to view their stats</span>
+  </span>
+</h1>
+<!--
 <div class="text-center" style="margin-top:70px">
     <p>Click on a skill position to view their stats</p>
 </div>
+-->
 
 <script>
   anime({
@@ -32,6 +40,24 @@ include "view-header.php";
   duration: 1000
 });
 </script>
+
+var textWrapper = document.querySelector('.text-center .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
 <?php
 include "view-footer.php";
